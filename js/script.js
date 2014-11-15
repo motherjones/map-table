be6144824fac37bf899cb3c9a650ec38bace305f
tasneem <traja@motherjones.com>
2014-11-14 20:44:59 -0800

    Major cleanup

--- js/script.js
+++ js/script.js
@@ -1,11 +1,46 @@
 
 var onDocumentReady = function() {
 
-    var public_spreadsheet_url = "https://docs.google.com/spreadsheet/pub?key=0AuHOPshyxQGGdDFnemtSV2tCXzJDOFNfeDNQY2lvb2c&output=html";
-
     Tabletop.init( { 
-    key: public_spreadsheet_url, callback: makeTable, simpleSheet: true,
-} )
+        key: 'https://docs.google.com/spreadsheet/pub?key=0AuHOPshyxQGGdDFnemtSV2tCXzJDOFNfeDNQY2lvb2c&output=html',
+        callback: makeTable, 
+        simpleSheet: true,
+    } )
+}
+
+/* 
+*  Create table with Tabletop data and apply appropriate classes
+*/
+var makeTable = function(data) {
+    var pymChild = null;
+    var $table = jQuery('#map_table');
+//    var $select = jQuery('#jump_to_state select');
+
+    for (i = 0; i < data.length; i++) {
+        var row = data[i];
+        var $tr = jQuery('<tr id="' + row['postal'] + '_row"></tr>');
+
+        //add state name
+        $tr.append('<td class="state_name">' + row['state'] + '</td>' );
+
+        $tr.append(makeCell(row, 'providestatus', 'providedetails', 'provide_map'));
+        $tr.append(makeCell(row, 'evidencestatus', 'evidencedetails', 'evidence_map'));
+        $tr.append(makeCell(row, 'judgesstatus', 'judgesdetails', 'judges_map'));
+        $tr.append(makeCell(row, 'fewerstatus', 'fewerdetails', 'fewer_map'));
+        $tr.append(makeCell(row, 'lawyersstatus', 'lawyersdetails', 'lawyers_map'));
+       
+        $table.append($tr);
+        
+//        $select.append('<option value="' + row['postal'] + '">' + row['state'] + '</option>');
+    }  
+
+/*    $select.change(function() {
+        window.location.hash = select.val() + '_row';
+        return false;
+    });*/
+
+    pymChild = new pym.Child();
+    pymChild.sendHeight();
 }
 
 /*
@@ -24,41 +59,4 @@ var makeCell = function(row, statusColumn, detailsColumn, mapName) {
     return '<td class="' + cls + '"><span>' + row[detailsColumn] + '</span></td>';
 }
 
-/* 
-*  Create table with Tabletop data and apply appropriate classes
-*/
-var makeTable = function(data) {
-    var pymChild = null;
-    var $table = jQuery('#map_table');
-    var $select = jQuery('#jump_to_state select');
-
-    for (i = 0; i < data.length; i++) {
-        var row = data[i];
-        var $tr = jQuery('<tr id="' + row['postal'] + '_row"></tr>');
-
-        //add state name
-        $tr.append('<td class="state_name">' + row['state'] + '</td>' );
-
-        $tr.append(makeCell(row, 'providestatus', 'providedetails', 'provide_map'));
-        $tr.append(makeCell(row, 'evidencestatus', 'evidencedetails', 'evidence_map'));
-        $tr.append(makeCell(row, 'judgesstatus', 'judgesdetails', 'judges_map'));
-        $tr.append(makeCell(row, 'fewerstatus', 'fewerdetails', 'fewer_map'));
-        $tr.append(makeCell(row, 'lawyersstatus', 'lawyersdetails', 'lawyers_map'));
-       
-        $table.append($tr);
-        
-        $select.append('<option value="' + row['postal'] + '">' + row['state'] + '</option>');
-    }  
-
-    $select.change(function() {
-        window.location.hash = select.val() + '_row';
-        return false;
-    });
-
-    pymChild = new pym.Child();
-    pymChild.sendHeight();
-}
-
 $(onDocumentReady);
-
-
